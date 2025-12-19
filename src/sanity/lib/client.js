35713c750f9d07b7d371cd8bad740d16b0a6382f
onurcemshn <onurcem154@gmail.com>
2025-12-19 14:39:03 +0300

    FIX: Remove missing env import and enable CDN

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,8 +1,9 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: 'zecf9mg9', // Senin ID'n
+  projectId: 'zecf9mg9', 
   dataset: 'production',
-  apiVersion: '2023-05-03', // Tarih formatı, standart kalabilir
-  useCdn: false, // Veriyi anlık çeksin diye false yaptık (Cache tutmasın)
+  apiVersion: '2023-05-03',
+  useCdn: true, // <--- HIZ İÇİN BURASI TRUE OLDU (CDN AÇIK)
+  perspective: 'published',
 })
